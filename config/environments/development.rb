a4beb35d86b64960248ddccec61355c0550bada5
EC2 Default User <ec2-user@ip-172-31-1-98.ec2.internal>
2022-05-17 14:16:01 +0000

    [途中]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "372e1a6a734e40d9963845e7369b0b16.vfs.cloud9.us-east-1.amazonaws.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -73,4 +74,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.active_job.queue_adapter=:inline
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+   devise_for :users
+   
+ resources:users,only:[:index,:show,:edit]
+ resources:books,only:[:new,:index,:show]
+ 
+  root to:'homes#top'
+ 
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
