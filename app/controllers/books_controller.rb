0b1a7c0d18b56985e244294d90adf3fb74bea40f
EC2 Default User <ec2-user@ip-172-31-1-98.ec2.internal>
2022-05-19 14:06:47 +0000

    アプリケーションを完成させよう課題２[途中]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,32 @@
 class BooksController < ApplicationController
   def new
+    @book=Book.new
   end
 
   def index
+    @books=Book.all
+    @book=Book.new
+    @user=current_user
   end
 
   def show
+    @book=Boook.find(params[:id])
   end
+
+  def create
+   @book=Book.new(book_params)
+   @book.save
+   redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+  def user_params
+    params.require(:user).permit(:name,:introduction,:profile_image)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
    devise_for :users
 
- resources:users,only:[:index,:show,:edit]
- resources:books,only:[:new,:index,:show]
+ resources:users,only:[:index,:show,:edit,:update]
+ resources:books,only:[:new,:index,:show,:create]
 
   root to:'homes#top'
   get '/home/about'=>'homes#about'
